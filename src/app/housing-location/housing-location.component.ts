59262a21638eed17afb889c0ee268ab8c88c3737
ahmad12nasser <ahmad749nasser123@gmail.com>
2023-09-27 23:07:18 +0300

    finish the all sessions for this tutorial from angular documentation

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousingLocation } from '../housinglocation'; 
+import { HousingLocation } from '../housinglocation';
+import { RouterModule, RouterLink, RouterOutlet } from '@angular/router'; // Import RouterModule here
+
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, RouterLink, RouterOutlet], // Add RouterModule to imports
   template: `
-  <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a> <!-- Use [routerLink] with a lowercase 'r' -->
+    </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
